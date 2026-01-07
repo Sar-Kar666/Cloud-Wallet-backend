d43e574208bc708b7c4bfb84e93eaeb5038d71ae
Diptanu Sarkar <diptanusarkar699@gmail.com>
2026-01-08 03:36:39 +0530

    added the txn sign logic

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const express = require ("express");
 const {userModel}=require("./models");
 const jwt= require("jsonwebtoken")
 const JWT_SECRET = process.env.JWT_SECRET;
-const { Keypair, PublicKey } = require("@solana/web3.js");
-
+const { Keypair, PublicKey, Transaction, Connection } = require("@solana/web3.js");
+const cors = require ("cors");
+const bs58 = require("bs58").default;
+const connection = new Connection("https://api.devnet.solana.com")
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+
+const privateKey= bs58.decode(process.env.SOLANA_PRIVATE_KEY)
+
+console.log(privateKey);
 
 app.post ("/api/v1/signup",(req,res)=>{
     const username = req.body.username;
@@ -50,11 +58,41 @@ app.post("/api/v1/signin",(req,res)=>{
    
 })
 
-app.post("/api/v1/txn/sign",(req,res)=>{
+
+
+app.post("/api/v1/txn/sign", async (req, res) => {
+  try {
+      
+      const serializedTx  = req.body.serializedTx;
+    // 1️⃣ Load private key (DO NOT hardcode in real apps)
+    const secretKey = privateKey;
+
+    const keypair = Keypair.fromSecretKey(secretKey);
+
+    // 2️⃣ Deserialize transaction
+    const tx = Transaction.from(
+      Buffer.from(serializedTx, "base64")
+    );
+
+    // 3️⃣ Sign transaction
+    tx.sign(keypair);
+
+
+    const signature = await connection.sendRawTransaction(
+      tx.serialize()
+    );
+
     res.json({
-        message: "sign the txn"
-    })
-})
+      message: "Transaction signed and sent",
+      signature
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
 
 app.get("/api/v1/txn",(req,res)=>{
     res.json({
